64b51b3319596310f45eacd2b3baa49ff1e075c6
Andrei Don <redimp@gmail.com>
2021-10-10 13:41:41 +0300

    Recreate db migrations and schema

--- db/migrate/20211007132118_fix_result_follows.decidim.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# This migration comes from decidim (originally 20180706111847)
-
-class FixResultFollows < ActiveRecord::Migration[5.2]
-  def change
-    Decidim::Follow.where(decidim_followable_type: "Decidim::Results::Result").update_all(decidim_followable_type: "Decidim::Accountability::Result")
-  end
-end
