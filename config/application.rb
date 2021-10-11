00f90e6e7dbe11b12b0b60c37ff28cdb4927fe8f
Andrei Don <redimp@gmail.com>
2021-10-11 14:12:29 +0300

    Set sidekiq as the job runner

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,13 @@ module Decidem
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults(6.0)
 
+    config.active_job.queue_adapter = :sidekiq
+
+    config.action_mailer.deliver_later_queue_name = nil # defaults to "mailers"
+    config.active_storage.queues.analysis   = nil       # defaults to "active_storage_analysis"
+    config.active_storage.queues.purge      = nil       # defaults to "active_storage_purge"
+    config.active_storage.queues.mirror     = nil       # defaults to "active_storage_mirror"
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
