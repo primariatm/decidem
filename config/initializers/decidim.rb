402e38ba79f9685bb0dde6e9ad963d86bc5ca0bb
Andrei Don <redimp@gmail.com>
2021-10-14 10:03:41 +0300

    Update email sender address config

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -5,7 +5,7 @@ Decidim.configure do |config|
   config.application_name = "Decidem"
 
   # The email that will be used as sender in all emails from Decidim
-  config.mailer_sender = "change-me@example.org"
+  config.mailer_sender = ENV["FROM_ADDRESS"]
 
   # Sets the list of available locales for the whole application.
   #
