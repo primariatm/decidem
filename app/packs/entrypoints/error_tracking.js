8a204cc270108f4b79f305f49a601805ae9aca2c
Raul Popadineti <raul@sunergos.ro>
2021-12-14 14:30:25 +0200

    Rename SENTRY_DSN env variable to SENTRY_BACKEND_DSN

--- app/packs/entrypoints/error_tracking.js
+++ app/packs/entrypoints/error_tracking.js
@@ -2,7 +2,7 @@ import * as Sentry from "@sentry/browser";
 import { Integrations } from "@sentry/tracing";
 
 Sentry.init({
-  dsn: window.SENTRY_DSN,
+  dsn: window.SENTRY_FRONTEND_DSN,
   environment: "production",
   integrations: [new Integrations.BrowserTracing()],
   tracesSampleRate: 0.2,
--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Sentry.init do |config|
-  config.dsn = ENV["SENTRY_DSN"]
+  config.dsn = ENV["SENTRY_BACKEND_DSN"]
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
   config.enabled_environments = %w[production]
 
