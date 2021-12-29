c052209225e5b3d5d0b0585c9c7f56c5e5bab2b0
Raul Popadineti <raul@sunergos.ro>
2021-12-29 21:43:23 +0200

    fix(webpacker): cleanup warnings

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,1 @@
-/* eslint-disable */
-
-module.exports = {
-  syntax: 'postcss-scss',
-  plugins: [
-    require('autoprefixer'),
-    require('postcss-import'),
-    require('postcss-flexbugs-fixes'),
-    require('postcss-preset-env')({
-      autoprefixer: {
-        flexbox: 'no-2009'
-      },
-      stage: 3
-    })
-  ]
-}
+module.exports = {}
