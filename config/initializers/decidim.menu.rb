ab935292f25fed17530bb033898c510f28d0a2fe
Raul Popadineti <raul@sunergos.ro>
2022-01-26 00:07:57 +0200

    fix(decidem): fix BP external URL

--- config/initializers/decidim.menu.rb
+++ config/initializers/decidim.menu.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 Decidim.menu(:menu) do |menu|
-  menu.add_item(:buget, "Bugetare participativă", "https://decidem-next.primariatm.dev/bp")
+  menu.add_item(:buget, "Bugetare participativă", "https://decidem.primariatm.ro/bp")
   menu.move(:buget, after: :assemblies)
 end
